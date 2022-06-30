428beb594e380d1374d1b643fa088226047e6e01
Kamil Sobik <kamil.sobik@gmail.com>
2022-06-30 23:06:13 +0200

    add table with user

--- app/request/show/route.js
+++ app/request/show/route.js
@@ -1,3 +1,2 @@
 import Route from '@ember/routing/route';
-
 export default class RequestShowRoute extends Route {}
--- app/users/route.js
+++ app/users/route.js
@@ -1,3 +1,23 @@
 import Route from '@ember/routing/route';
 
-export default class UsersRoute extends Route {}
+export default class UsersRoute extends Route {
+  model() {
+    const users = [
+      {
+        id: 1,
+        username: 'admin',
+        password: 'admin123',
+        email: 'admin@admin.com',
+        isRequestor: true,
+      },
+      {
+        id: 2,
+        username: 'user',
+        password: 'user123',
+        email: 'user@user.com',
+        isRequestor: false,
+      },
+    ];
+    return users;
+  }
+}
