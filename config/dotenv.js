6a1aa69d57ff63348fc55819856d301d0aff8a5a
Kamil Sobik <kamil.sobik@gmail.com>
2022-08-06 11:43:06 +0200

    fix problem with dotenv configuration on netlify

---
+++ config/dotenv.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+
+'use strict';
+
+const path = require('path');
+
+module.exports = function (env) {
+  return {
+    clientAllowedKeys: ['AUTH0_CLIENT_ID', 'AUTH0_DOMAIN'],
+    fastbootAllowedKeys: [],
+    failOnMissingKey: false,
+    path: path.join(path.dirname(__dirname), '.env'),
+    enabled: env !== 'production',
+  };
+};
