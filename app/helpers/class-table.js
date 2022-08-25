4fb327a3afdcf2feb98ba07ba0b026f49f1451c6
Kamil Sobik <kamil.sobik@gmail.com>
2022-08-25 15:03:01 +0200

    add helper for styling Requests

---
+++ app/helpers/class-table.js
@@ -0,0 +1,16 @@
+import { helper } from '@ember/component/helper';
+
+function classTable([typeRequest]) {
+  console.log(typeRequest);
+  let newTableClass = 'table-primary';
+  if (typeRequest === '1.0 tpm') {
+    newTableClass = 'table-success';
+  } else if (typeRequest === '2.0 fault') {
+    newTableClass = 'table-warning';
+  } else if (typeRequest === '3.0 breakdown') {
+    newTableClass = 'table-danger';
+  }
+  return newTableClass;
+}
+
+export default helper(classTable);
