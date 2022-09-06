e64c68aa823daded751880ecfd8a8ede67c0fb7f
Kamil Sobik <kamil.sobik@gmail.com>
2022-09-06 22:36:05 +0200

    add fontawsome package

--- app/components/close-request/component.js
+++ app/components/close-request/component.js
@@ -3,7 +3,7 @@ import Component from '@glimmer/component';
 export default class CloseRequestComponent extends Component {
   get closedfilteredRequests() {
     return this.args.model
-      .filter(({ status }) => status === 'closed')
+      .filter(({ status }) => status === 'open')
       .sortBy('type', 'createdAt')
       .reverse();
   }
