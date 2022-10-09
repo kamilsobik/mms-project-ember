eaa61ea4eb84f9bd8d782af016e7dde0498edc19
Kamil Sobik <kamil.sobik@gmail.com>
2022-10-09 22:43:12 +0200

    add function to changing user type

---
+++ app/components/modal/edit-user/component.js
@@ -0,0 +1,30 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class ModalEditUserComponent extends Component {
+  @action
+  onUserTypeChange(property, { target: { value } }) {
+    this.args.user[property] = value;
+  }
+
+  @action
+  onPasswordChange(event) {
+    this.args.user.password = event.target.value;
+  }
+
+  @action
+  onEmailChange(event) {
+    this.args.user.email = event.target.value;
+  }
+
+  @action
+  async onSaveChange() {
+    await this.args.user.save();
+    this.args.onClose();
+  }
+  @action
+  onCancel() {
+    this.args.user.rollbackAttributes();
+    this.args.onClose();
+  }
+}
---
+++ app/components/user-table/user-table-row/component.js
@@ -0,0 +1,16 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+
+export default class UserTableUserTableRow extends Component {
+  @tracked isShowUserModal = false;
+
+  @action
+  onShowUserModal() {
+    this.isShowUserModal = true;
+  }
+  @action
+  onHideUserModal() {
+    this.isShowUserModal = false;
+  }
+}
