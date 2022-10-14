b76c5c3d2eda78a281bbc3ee5b4bc374252feb70
Kamil Sobik <kamil.sobik@gmail.com>
2022-10-14 13:33:08 +0200

    changing shared::button - add get to change size

--- app/components/shared/button/component.js
+++ app/components/shared/button/component.js
@@ -1,9 +1,15 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 
-export default class FormGeneratorButtonComponenet extends Component {
+export default class SharedButtonComponenet extends Component {
   get type() {
     return this.args.type || 'button';
+  }
+
+  get size() {
+    const { size } = this.args;
+    const isCorrectSize = ['sm', 'md', 'lg'].includes(size);
+    return isCorrectSize ? `btn-${size}` : '';
   }
 
   @action
