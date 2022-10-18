6eefb80deb77902c4d64b7bafbc869ff469f9d77
Kamil Sobik <kamil.sobik@gmail.com>
2022-10-18 09:14:24 +0200

    modify class helper

--- app/helpers/class-table.js
+++ app/helpers/class-table.js
@@ -1,11 +1,15 @@
 import { helper } from '@ember/component/helper';
-const tableClass = {
-  '1.0 tpm': 'table-success',
-  '2.0 fault': 'table-warning',
-  '3.0 breakdown': 'table-danger',
-};
+const tableClasses = [
+  { value: '1.0 tpm', class: 'table-success' },
+  { value: '2.0 fault', class: 'table-warning' },
+  { value: '3.0 breakdown', class: 'table-danger' },
+];
 
 export function classTable([typeRequest]) {
-  return tableClass[typeRequest] || 'table-primary';
+  return (
+    tableClasses.find(({ value }) => value === typeRequest).class ||
+    'table-primary'
+  );
 }
+
 export default helper(classTable);
