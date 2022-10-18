3b4f7cf30805cc7619a186943059afc91d47302d
Kamil Sobik <kamil.sobik@gmail.com>
2022-10-18 09:04:30 +0200

    add new propert to shared::button in app

--- app/components/shared/button/component.js
+++ app/components/shared/button/component.js
@@ -12,6 +12,22 @@ export default class SharedButtonComponenet extends Component {
     return isCorrectSize ? `btn-${size}` : '';
   }
 
+  get variant() {
+    const { variant } = this.args;
+    const isCorrectVariant = [
+      'primary',
+      'secondary',
+      'success',
+      'danger',
+      'warning',
+      'info',
+      'light',
+      'dark',
+      'link',
+    ].includes(variant);
+    return isCorrectVariant ? `btn-${variant}` : '';
+  }
+
   @action
   onClick() {
     this.args.onClick?.(...arguments);
