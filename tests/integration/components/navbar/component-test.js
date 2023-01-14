cac189fb99dcceaff8a6b7ea99cf4c1b0cfc379b
Kamil Sobik <kamil.sobik@gmail.com>
2023-01-14 22:33:00 +0100

    add tests for navbar

--- tests/integration/components/navbar/component-test.js
+++ tests/integration/components/navbar/component-test.js
@@ -6,21 +6,48 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | navbar', function (hooks) {
   setupRenderingTest(hooks);
 
-  test.skip('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  test('display correct routes for users responder type', async function (assert) {
+    const sessionService = this.owner.lookup('service:session');
+    sessionService.set('currentUser', { userType: 'responder' });
 
-    await render(hbs`<Navbar />`);
+    await render(hbs`<Navbar/>`);
+    assert.dom('[data-test-main-link]').hasText('Main page');
+    assert.dom('[data-test-requests-link]').hasText('Requests');
+    assert.dom(this.element).doesNotIncludeText('Open new request');
+    assert.dom('[data-test-close-request-link]').hasText('Close request');
+    assert.dom(this.element).doesNotIncludeText('Users');
+    assert.dom('[data-test-kpi-link]').hasText('KPI');
+    assert.dom('[data-test-archive-link]').hasText('Archive');
+    assert.dom('[data-test-logout-link]').hasText('Logout');
+  });
 
-    assert.dom(this.element).hasText('');
+  test('display correct routes for users requestor type', async function (assert) {
+    const sessionService = this.owner.lookup('service:session');
+    sessionService.set('currentUser', { userType: 'requestor' });
 
-    // Template block usage:
-    await render(hbs`
-      <Navbar>
-        template block text
-      </Navbar>
-    `);
+    await render(hbs`<Navbar/>`);
+    assert.dom('[data-test-main-link]').hasText('Main page');
+    assert.dom('[data-test-requests-link]').hasText('Requests');
+    assert.dom('[data-test-open-request-link]').hasText('Open new request');
+    assert.dom(this.element).doesNotIncludeText('Close request');
+    assert.dom(this.element).doesNotIncludeText('Users');
+    assert.dom('[data-test-kpi-link]').hasText('KPI');
+    assert.dom('[data-test-archive-link]').hasText('Archive');
+    assert.dom('[data-test-logout-link]').hasText('Logout');
+  });
 
-    assert.dom(this.element).hasText('template block text');
+  test('display correct routes for users admin type', async function (assert) {
+    const sessionService = this.owner.lookup('service:session');
+    sessionService.set('currentUser', { userType: 'admin' });
+
+    await render(hbs`<Navbar/>`);
+    assert.dom('[data-test-main-link]').hasText('Main page');
+    assert.dom('[data-test-requests-link]').hasText('Requests');
+    assert.dom('[data-test-open-request-link]').hasText('Open new request');
+    assert.dom('[data-test-close-request-link]').hasText('Close request');
+    assert.dom('[data-test-users-link]').hasText('Users');
+    assert.dom('[data-test-kpi-link]').hasText('KPI');
+    assert.dom('[data-test-archive-link]').hasText('Archive');
+    assert.dom('[data-test-logout-link]').hasText('Logout');
   });
 });
